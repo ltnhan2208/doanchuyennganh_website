38e6166b7d3adea06d09bf2f9cc0ed314c5c0305
ltnhan2208 <65239814+ltnhan2208@users.noreply.github.com>
2020-12-25 15:13:48 +0700

    Update connection.php

--- doan_cn/includes/connection.php
+++ doan_cn/includes/connection.php
@@ -1,8 +1,8 @@
 <?php 
-	$server_username 	= "id13992311_trungnhan";
-	$server_password 	= "DH51700752_nhan";
+	$server_username 	= "root";
+	$server_password 	= "";
 	$server_host		= "localhost";
-	$database			= "id13992311_qlmaytinh";
+	$database		= "qlmaytinh";
 
 	$conn = mysqli_connect($server_host, $server_username, $server_password, $database) or die ("Can't connect to database");
 	mysqli_query($conn,"SET NAMES 'UTF8'");
